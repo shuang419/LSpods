2931f89624fe2c2ccc81534bfefecaf7d7b5f848
apple <apple@liangshuang.local>
2018-01-30 13:56:34 +0800

    加入新建的项目

---
+++ text/text/LSView.h
@@ -0,0 +1,13 @@
+//
+//  LSView.h
+//  text
+//
+//  Created by apple on 2018/1/30.
+//  Copyright © 2018年 apple. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface LSView : UIView
+
+@end
---
+++ text/text/LSView.m
@@ -0,0 +1,22 @@
+//
+//  LSView.m
+//  text
+//
+//  Created by apple on 2018/1/30.
+//  Copyright © 2018年 apple. All rights reserved.
+//
+
+#import "LSView.h"
+
+@implementation LSView
+
+////////测试
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
